c3cc4e5ede43f3e51680e825dca27fcaffb2fc67
Sunny Le Bunz <msafinch@gmail.com>
2024-06-04 13:58:39 -0400

    added functionality to light and dark mode buttons

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# Bunz-the-Blogger
+# Bunz The Blogger
 
+## Description
+
+Blog weebsite for the gal that might just have something to say.<br>
+Upon loading the page, you will be met with a form that will allow you to add a blog submission
 
 ## Links
 
 <a href="https://github.com/0-Sunny-0/Bunz-the-Blogger">GitHub Repo</a><br>
 <a href="https://0-sunny-0.github.io/Bunz-the-Blogger/">Deployed Link</a>
-
-## Description
-
-Blog weebsite for the gal that might just have something to say. 
-
+ 
 ## Visuals
 
 <img src="./assets/images/My-landing-page.png" alt="screenshot of blog landing page"/>
---
+++ assets/js/blog.js
@@ -0,0 +1,34 @@
+// Light and dark mode toggle function
+let modeChange = document.getElementsByClassName('mode')
+
+
+for (let i = 0; i < modeChange.length; i++) {
+    const element = modeChange[i];
+
+    element.addEventListener('click', function(event){
+        console.log(event.target.value);
+        if (event.target.value ==='dark') {
+            document.body.classList.add('dark')
+            document.body.classList.remove('light')
+        }else{
+            document.body.classList.add('light')
+            document.body.classList.remove('dark')
+        }
+    })
+    
+}
+
+
+
+// The submit button function and saving to local storage
+function myClickFunction(event) {
+    // console.log(event);
+    document.getElementById("blogForm").submit();
+    event.preventDefault();
+    localStorage.setItem("username", "");
+    localStorage.setItem("title", "");
+    localStorage.setItem("content", "");
+}
+
+
+// localStorage.setItem("username", "");
