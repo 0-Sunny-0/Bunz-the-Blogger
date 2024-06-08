2d922232fad103af224d80e0b6d30ea3ff4e9fb1
Sunny Le Bunz <msafinch@gmail.com>
2024-06-08 14:35:52 -0400

    added light/dark mode function to logic.js.
    changed location of light/dark mode function to logic.js so that both the form and blog page would respond accordingly, instead of having them on separate files. Also added css properties for the blog page, so that light and dark mode would toggle correctly.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,18 +1,18 @@
 // Light and dark mode toggle function
-let modeChange = document.getElementsByClassName('mode')
+// let modeChange = document.getElementsByClassName('mode')
 
-for (let i = 0; i < modeChange.length; i++) {
-    const element = modeChange[i];
+// for (let i = 0; i < modeChange.length; i++) {
+//     const element = modeChange[i];
 
-    element.addEventListener('click', function(event){
-        console.log(event.target.value);
-        if (event.target.value ==='dark') {
-            document.body.classList.add('dark')
-            document.body.classList.remove('light')
-        }else{
-            document.body.classList.add('light')
-            document.body.classList.remove('dark')
-        }
-    })
+//     element.addEventListener('click', function(event){
+//         console.log(event.target.value);
+//         if (event.target.value ==='dark') {
+//             document.body.classList.add('dark')
+//             document.body.classList.remove('light')
+//         }else{
+//             document.body.classList.add('light')
+//             document.body.classList.remove('dark')
+//         }
+//     })
     
-}
+// }
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,22 +1,24 @@
 // Light and dark mode toggle function
-let modeChange = document.getElementsByClassName('mode')
+// let modeChange = document.getElementsByClassName('mode')
 
+// Added the below function to the logic.js file, since I want both
+// form and blog to do the same thing. 
 
-for (let i = 0; i < modeChange.length; i++) {
-    const element = modeChange[i];
+// for (let i = 0; i < modeChange.length; i++) {
+//     const element = modeChange[i];
 
-    element.addEventListener('click', function(event){
-        console.log(event.target.value);
-        if (event.target.value ==='dark') {
-            document.body.classList.add('dark')
-            document.body.classList.remove('light')
-        }else{
-            document.body.classList.add('light')
-            document.body.classList.remove('dark')
-        }
-    })
+//     element.addEventListener('click', function(event){
+//         console.log(event.target.value);
+//         if (event.target.value ==='dark') {
+//             document.body.classList.add('dark')
+//             document.body.classList.remove('light')
+//         }else{
+//             document.body.classList.add('light')
+//             document.body.classList.remove('dark')
+//         }
+//     })
     
-}
+// }
 
 
 
