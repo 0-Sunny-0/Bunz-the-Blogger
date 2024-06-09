7ce34a8a99fdd3b53d9fbb524e30e218a810382a
Sunny Le Bunz <msafinch@gmail.com>
2024-06-09 12:47:59 -0400

    added functionality for alert prompt
    alert prompt has been added to function that creates the local storage. Attempted to do it outside of the event listener/local storage but wouldn't respond. simplier option inside of local storage function.

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,7 +3,6 @@
 const blogForm = document.querySelector('#blogForm');
 const submit = document.querySelector('#submit');
 
-
 // Event listener for submission click. Be sure to include prevent default. 
 // Check console log to ensure form information has been saved. 
 
@@ -13,16 +12,43 @@ submit.addEventListener('click', function(event) {
     const title = blogForm.querySelector('#title').value;
     const content = blogForm.querySelector('#content').value;
 
+    if (username === '' || title === '' || content === '') {
+                alert('Aht Aht! Fill out those fields big dawg.'); 
+                return;
+    };
+
     const formData = {
         username: username,
         title: title,
         content: content
     }; 
-
+    
     const formDataString = JSON.stringify(formData);
 
     localStorage.setItem('formData', formDataString);
     console.log('Form data has been saved to local storage.');
+
 });
+
+// Function to validate if form fields have been filled out. 
+// ended up adding this to the above function. The variables were off, which 
+// was causing the function to....not function (slaps knee)
+
+// document.getElementById('blogForm').addEventListener("submit", validateForm);
+// function validateForm() {
+//     const username = document.getElementById('#username').value;
+//     const title = document.getElementById('#title').value;
+//     const content = document.getElementById('#content').value;
+
+//     if (username === '' || title === '' || content === '') {
+//         alert('Aht Aht! Fill out those fields big dawg.'); 
+//         return;
+//     };
+// };
+
+
+// function to retrieve data from local storage
+
+const posts = localStorage.getItem('formData');
 
 
